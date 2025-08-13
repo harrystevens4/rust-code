7b99a203dfaaec340ec04be8d86bf59610751885
rufus173 <derrickotron5000@gmail.com>
2025-08-13 13:02:12 +0100

    fully funcional

--- simple-hash/src/main.rs
+++ simple-hash/src/main.rs
@@ -1,27 +1,65 @@
 use std::io;
+use std::cmp;
+use std::io::Read;
+use std::env;
 
 fn main() {
-    let hash_length = 10;
-    let mut text = String::new();
-    let mut hash_string = String::new();
-    io::stdin()
-    	.read_line(&mut text)
-    	.expect("Could not get stdin");	
-    text.truncate(text.len() - 1);//remove trailing newline
-    println!("Hashing {text}");
-    let text_length = text.len();
-    let mut text_index = 0;
-    for hash_index in 0..hash_length {
-    	println!("copying {:?} to {hash_index}",text.chars().nth(text_index));
-	let mut current_char: char = text
-	    .chars()
-	    .nth(text_index)
-	    .expect("could not index string");
-	hash_string.push(current_char);
-	text_index += 1;
-	if text_index == text_length { //wrap around to 0
-	    text_index = 0;
+	let inputs = {
+		let args = env::args().collect::<Vec<_>>();
+		if args.len() > 1 {
+			(&args[1..]).to_owned()
+		}else {
+			let mut input = String::new();
+			let _ = io::stdin().read_to_string(&mut input);
+			vec![input]
+		}
+	};
+	inputs.into_iter().for_each(
+		|i| println!("{}",hash(i.to_string())
+			.iter()
+			.map(|x| format!("{:x}",x))
+			.collect::<String>()
+		)
+	)
+}
+
+fn rotate(input: &mut String,count: isize){
+	if input.len() == 0 {return}
+	let bound = 
+		if count < 0 {
+			input.len() - ((-count as usize) % input.len()) as usize
+		}else {
+			count as usize
+		};
+	input.extend_from_within(..(bound % input.len()));
+	let _ = input.drain(..(bound % input.len()));
+}
+
+fn as_chunks<'a>(string: &'a String,chunk_size: usize) -> Vec<&'a str>{
+	let mut chunks: Vec<&str> = vec![];
+	for i in 0..(string.len().div_ceil(chunk_size)){
+		let start = i * chunk_size;
+		let end = cmp::min(start + chunk_size,string.len());
+		chunks.push(&string[start..end]);
 	}
-    }
-    println!("hash: {hash_string}");
+	chunks
+}
+
+fn hash(input: String) -> [u8; 8] {
+	let mut chunks: Vec<[u8; 8]> = vec![];
+	let mut result = [0_u8; 8];
+	for (i,chunk) in as_chunks(&input,8).into_iter().enumerate(){
+		let mut buffer = chunk.to_string();
+		rotate(&mut buffer,i as isize);
+		let str_chunk = <String as AsRef<[u8]>>::as_ref(&buffer);
+		let mut processed_chunk = [0; 8];
+		processed_chunk[..str_chunk.len()].copy_from_slice(str_chunk);
+		chunks.push(processed_chunk);
+	}
+	for chunk in chunks {
+		let new_result: Vec<u8> = chunk.iter().zip(result).map(|(x,y)| x ^ y).collect();
+		result.copy_from_slice(&new_result[..]);
+	}
+	//chunks.iter().for_each(|x| println!("{:?}",x));
+	result
 }
