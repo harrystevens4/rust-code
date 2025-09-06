891851418d3a2bed66f9bdfbafb5ea5ea2f2e519
rufus173 <derrickotron5000@gmail.com>
2025-09-06 23:10:04 +0100

    added some command line options (help, colour and depth)

--- sl/src/main.rs
+++ sl/src/main.rs
@@ -2,29 +2,73 @@ use std::path::Path;
 use std::os::unix::fs::MetadataExt;
 use std::ffi::OsString;
 use std::env;
+use args::Args;
+use args::ArgError::*;
 
 #[link(name = "term")]
 unsafe extern "C" {
 	fn get_term_width() -> i32;
 }
 
+struct Config {
+	colour: bool,
+	max_depth: usize,
+}
+
+fn print_help(){
+	println!("usage: {} [options] [path] ...",env::args().next().unwrap());
+	println!("options:");
+	println!("	-h, --help      : print help");
+	println!("	-n, --no-colour : turn off colour");
+	println!("	-d, --depth <n> : the maximum depth to look through");
+}
 fn main() {
-	let mut dirs = env::args()
-		.enumerate()
-		.filter(|&(i,_)| i != 0) //skip argv[0]
-		.map(|x| x.1)
-		.collect::<Vec<_>>();
-		if dirs.len() == 0 {dirs.push(".".into())}
+	let mut config = Config {
+		colour: true,
+		max_depth: usize::MAX,
+	};
+	//====== parse arguments ======
+	let format = vec![
+	//       short      long               parameter
+		(Some("h"), Some("help"),      false    ),
+		(Some("n"), Some("no-colour"), false    ),
+		(Some("d"), Some("depth"),     true     ),
+	];
+	let args = match Args::new(env::args().collect(),format){
+		Ok(args) => args,
+		Err(e) => match e {
+			UnknownArgument(t) => {
+				eprintln!("Error: unknown argument {:?}",t);
+				return;
+			},
+			MissingParameter(t) => {
+				eprintln!("Error: missing parameter to {:?}",t);
+				return;
+			}
+		}
+	};
+	if args.has_short("h") || args.has_long("help") {
+		print_help();
+		return;
+	}
+	if args.has_short("n") || args.has_long("no-colour") {config.colour = false}
+	if let Some(depth_arg) = args.get_arg(Some("d"),Some("depth")) {config.max_depth = depth_arg.parse().unwrap_or(usize::MAX)}
+	//====== print_dir for each path given ======
+	let mut dirs = args.other[1..].to_vec(); //exclude argv[0]
+	if dirs.len() == 0 {dirs.push(".".into())}
 	for arg in dirs{
 		let dir = Path::new(&arg);
 		println!("{}",match dir.file_name().unwrap_or(&OsString::from("..")).to_str(){
 			Some(name) => name,
 			None => {eprintln!("Path has bad unicode data"); continue},
 		});
-		if dir.exists() {print_dir(dir,"".into())}
+		if dir.exists() {print_dir(dir,"".into(),0,&config)}
 	}
 }
-fn print_dir(path: &Path, indent: String){
+fn print_dir(path: &Path, indent: String, depth: usize, config: &Config){
+	//dont excede max depth
+	if depth > config.max_depth {return}
+	//====== initialise things ======
 	let width = unsafe {get_term_width()};
 	let items = match path.read_dir(){
 		Ok(d) => d,
@@ -41,7 +85,10 @@ fn print_dir(path: &Path, indent: String){
 						Ok(name) => name,
 						Err(e) => {eprintln!("{:?}: Bad unicode data",e); String::new()},
 					};
-					let pretty_file_name = format!("{}{}\x1b[39;49m",get_highlight(<&Path>::from(&item_path)),file_name);
+					//====== colour filename if enabled ======
+					let pretty_file_name = 
+						if config.colour {format!("{}{}\x1b[39;49m",get_highlight(<&Path>::from(&item_path)),file_name)}
+						else {file_name};
 					//====== print the ones with ├ ======
 					if i != items.len()-1 {print_clamped(format!("{}├─{}",&indent,pretty_file_name),width)}
 					//====== the last one should use └ ======
@@ -49,7 +96,7 @@ fn print_dir(path: &Path, indent: String){
 					if item_path.is_dir() {print_dir(&item_path.as_path(),indent.clone()+
 						if i == items.len()-1 {"  "}
 						else {"│ "}
-					)}
+					,depth+1,config)}
 				},
 				Err(e) => eprintln!("{:?}",e),
 			}
