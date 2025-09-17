15d3bb1db0f9ab6c77a958d3edeb84f2140a92f5
rufus173 <derrickotron5000@gmail.com>
2025-09-17 21:23:21 +0100

    moved config file parser to seperate library

--- tmux-service-manager/src/main.rs
+++ tmux-service-manager/src/main.rs
@@ -1,5 +1,6 @@
 #![feature(trim_prefix_suffix)]
 use args::Args;
+use iniconfig::ConfigFile;
 use std::collections::HashMap;
 use std::fs;
 use std::env;
@@ -68,57 +69,10 @@ fn main() -> ExitCode {
 			return ExitCode::FAILURE
 		}
 	};
-	let config = parse_config(&config_file);
+	let config = ConfigFile::from(config_file.as_str());
 	println!("{:?}",config);
 
 	//====== success ======
 	ExitCode::SUCCESS
 }
 
-fn parse_config(raw_config: &str) -> Vec<Service>{
-	let config_data: Vec<Service> = vec![];
-	let lines = raw_config.split('\n').collect::<Vec<&str>>();
-	let mut services = read_label("top_level",lines.into_iter());
-	//the first "service" will be everything before the first label, so strip it off
-	services.remove(0);
-	services
-}
-fn is_label(line: &str) -> bool {
-	if line.trim().chars().nth(0) == Some('[') {return true}
-	false
-}
-fn extract_label(line: &str) -> String {
-	line.trim().trim_prefix("[").trim_suffix("]").into()
-}
-fn read_label<'a>(name: &str, mut lines: impl Iterator<Item = &'a str>) -> Vec<Service>{
-	let mut service = Service {
-		properties: HashMap::new(),
-		name: name.into(),
-	};
-	//====== for line in lines ======
-	loop {
-		//the flipping for loop calls into_iterator which moves. SO ANOYING
-		let line = match lines.next() { Some(v) => v, None => break, };
-		//====== if it is a valid property, add it to the hashmap ======
-		if is_property(line){
-			let (key, value) = extract_property(line);
-			let _ = service.properties.insert(key,value);
-		}
-		//====== recurse if another label is reached ======
-		if is_label(line){
-			let service_name = extract_label(line);
-			let mut result = vec![service];
-			result.append(&mut read_label(&service_name,lines));
-			return result
-		}
-	}
-	//base case
-	vec![service]
-}
-fn is_property(line: &str) -> bool {
-	line.contains('=')
-}
-fn extract_property(line: &str) -> (String,String){
-	let result = line.split('=').take(2).collect::<Vec<_>>();
-	(result[0].into(), result[1].into())
-}
