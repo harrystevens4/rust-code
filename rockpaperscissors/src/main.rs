3260b90df4496fe8d1e38f159af3aac117231ca6
rufus173 <rufus09173@gmail.com>
2025-10-04 15:17:22 +0100

    yeah clue is in the name with this one

---
+++ rockpaperscissors/src/main.rs
@@ -0,0 +1,74 @@
+const ROCK_ASCII: &str = 
+"
+           __ __
+      _-__/  -  \\
+     /       -   |
+    |  ..        |
+    |  .         \\
+    |        --  /
+    \\  -     -  /
+     |      ___/
+     \\__-__/
+";
+const PAPER_ASCII: &str = 
+"
+       ____________
+      /|          |
+     / |~~~~~     |
+    /  |          |
+   /   |~~~~~~~   |
+  /    |          |
+ /-----/~~~~~     |
+ |                |
+ | ~~~~~~~~~      |
+ |                |
+ | ~~~~~~~~~~~~   |
+ |                |
+ |----------------|
+";
+const SCISSORS_ASCII: &str =
+"
+           ____
+    ____  /    \\
+   /    \\ |    |
+   |    | \\____/
+   \\____/ o
+          |\\
+          | \\
+          |  \\
+          |   \\
+          |    \\
+          |     \\
+          |
+";
+const ART: [&str; 3] = [
+	ROCK_ASCII,PAPER_ASCII,SCISSORS_ASCII
+];
+
+fn main() -> io::Result<()>{
+	use std::io;
+	use rand::{Rng,rng};
+    println!("Enter rock, paper or scissors");
+	loop {
+		let computer_choice: u8 = rng().random_range(0..3);
+		let mut line = String::new();
+		io::stdin().read_line(&mut line)?;
+		match line.as_str().trim() {
+			"rock"     | "r" => compare_win(0,computer_choice),
+			"paper"    | "p" => compare_win(1,computer_choice),
+			"scissors" | "s" => compare_win(2,computer_choice),
+			_ => {
+				println!("unrecognised word");
+				continue;
+			},
+		}
+	}
+}
+fn compare_win(player_choice: u8, computer_choice: u8){
+	println!("====== You chose ======\n{}",ART[player_choice as usize]);
+	println!("====== I chose ======\n{}",ART[computer_choice as usize]);
+	print!("---> Therefore, ");
+	if player_choice == (computer_choice+1)%3 {println!("you win")}
+	else if player_choice == computer_choice {println!("we raw")}
+	else {println!("you lose")}
+}
