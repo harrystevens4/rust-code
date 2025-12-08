e0d451b6d9c3e9ce833d6dcb9a1cb7819ac7d76e
rufus173 <rufus09173@gmail.com>
2025-12-08 11:05:16 +0000

    now opens leverfile file

--- lever/leverfile.rs
+++ lever/leverfile.rs
@@ -1,4 +1,7 @@
 use std::path::Path;
+use std::io::Read;
+use std::fs::File;
+use std::io;
 
 pub struct LeverFile {
 	url: Option<String>,
@@ -8,19 +11,22 @@ pub struct LeverFile {
 }
 
 impl LeverFile {
-	fn load(path: &Path) -> Self {
+	pub fn load<T: AsRef<Path>>(path: T) -> io::Result<Self> {
 		//defaults
 		let url = None;
 		let always_copy_files = vec![];
 		let copy_once_files = vec![];
-		let compile_command = String::new()
+		let compile_command = String::new();
 		//load file
+
+		let mut leverfile = String::new();
+		let _ = File::open(path.as_ref())?.read_to_string(&mut leverfile);
 		//pack struct
-		Self {
+		Ok(Self {
 			url,
 			always_copy_files,
 			copy_once_files,
 			compile_command
-		}
+		})
 	}
 }
--- lever/main.rs
+++ lever/main.rs
@@ -1,4 +1,9 @@
+mod leverfile;
+
 use std::error::Error;
+use leverfile::LeverFile;
+
 fn main() -> Result<(),Box<dyn Error>> {
+	LeverFile::load("leverfile")?;
 	Ok(())
 }
