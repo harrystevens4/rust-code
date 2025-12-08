e77d9cbe0630951ea09b82125059b2fc391b21d7
rufus173 <derrickotron5000@gmail.com>
2025-12-08 20:45:25 +0000

    new compile function runs compile commands

--- lever/leverfile.rs
+++ lever/leverfile.rs
@@ -1,4 +1,5 @@
 use std::path::Path;
+use std::process::{Stdio,Command};
 use std::io::Read;
 use std::fs::File;
 use std::io;
@@ -10,12 +11,14 @@ pub struct LeverFile {
 	url: Option<String>,
 	compile_commands: Vec<String>,
 	install_commands: Vec<String>,
+	name: String,
 }
 
 #[derive(Debug)]
 pub enum LeverFileError {
 	UnknownSection((String,usize)),
 	IoError(std::io::Error),
+	MissingSection(String),
 }
 
 impl Error for LeverFileError {}
@@ -25,6 +28,7 @@ impl Display for LeverFileError {
 		match self {
 			UnknownSection((section,line)) => write!(f, "Unknown section {section} at line {line})"),
 			IoError(error) => write!(f, "{error:?})"),
+			MissingSection(section) => write!(f, "Missing section {section:?})"),
 		}
 	}
 }
@@ -33,6 +37,7 @@ impl LeverFile {
 	pub fn load<T: AsRef<Path>>(path: T) -> Result<Self,LeverFileError> {
 		//defaults
 		let mut url = None;
+		let mut name = String::new();
 		let mut compile_commands = vec![];
 		let mut install_commands = vec![];
 		//load file
@@ -65,16 +70,69 @@ impl LeverFile {
 				"compile" => {
 					compile_commands.push(line.into())
 				}
+				"name" => {
+					name = line.into();
+				}
 				_ => {
 					return Err(LeverFileError::UnknownSection((section_name,line_numer+1)))
 				}
 			}
+		}
+		//check for required fields
+		if name.len() == 0 {
+			return Err(LeverFileError::MissingSection("name".into()))
 		}
 		//pack struct
 		Ok(Self {
 			url,
 			compile_commands,
 			install_commands,
+			name,
 		})
+	}
+	pub fn compile<T: AsRef<Path>>(&self, path: T) -> io::Result<()>{
+		println!("=== Compiling {} ===",self.name);
+		for command in self.compile_commands.iter() {
+			//start a shell to execute each line
+			let mut child = Command::new("sh")
+				.arg("-c")
+				.arg(command)
+				.stderr(Stdio::piped())
+				.stdout(Stdio::piped())
+				.stdin(Stdio::null())
+				.spawn()?;
+			println!("> {command}");
+			//read and print stdout from shell
+			if let Some(ref mut stdout) = child.stdout {
+				let mut line_buffer = String::new();
+				loop {
+					//read some data
+					let mut stdout_buffer = [0; 64];
+					let count = stdout.read(&mut stdout_buffer)?;
+					if count == 0 {break}
+					//output in a nice format
+					for byte in &stdout_buffer[..count] {
+						let ch = char::from(*byte);
+						if ch == '\n' {
+							println!("==> {}",line_buffer);
+							line_buffer.truncate(0);
+						}else {
+							line_buffer.push(ch);
+						}
+					}
+				}
+			}
+			let exit_status = child.wait()?;
+			if !exit_status.success() {
+				eprintln!("Process exited with an error");
+				match exit_status.code() {
+					Some(code) => println!("{command:?} exited with status {code}",),
+					None => println!("{command:?} was terminated by signal",),
+				}
+				return Err(io::Error::other("Compilation error"))
+			}
+		}
+		println!("Successfull compile");
+		Ok(())
 	}
 }
--- lever/main.rs
+++ lever/main.rs
@@ -1,10 +1,11 @@
 mod leverfile;
 
 use std::error::Error;
-use leverfile::LeverFile;
+use leverfile::{LeverFile};
 
 fn main() -> Result<(),Box<dyn Error>> {
 	let leverfile = LeverFile::load("leverfile")?;
 	println!("{:?}",leverfile);
+	leverfile.compile(".")?;
 	Ok(())
 }
