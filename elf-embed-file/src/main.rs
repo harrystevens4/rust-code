10bbfd4d901f75e445fcf084587823d3d5374d40
rufus173 <derrickotron5000@gmail.com>
2026-01-13 16:49:03 +0000

    switched to using seperate data and data_size symbols

--- elf-embed-file/src/main.rs
+++ elf-embed-file/src/main.rs
@@ -2,7 +2,7 @@ use target_lexicon::Triple;
 use faerie::artifact::*;
 use args::Args;
 use std::error::Error;
-use std::fs::File;
+use std::fs::{File,read};
 
 fn main() -> Result<(),Box<dyn Error>> {
 	//====== get file names from command line ======
@@ -12,6 +12,10 @@ fn main() -> Result<(),Box<dyn Error>> {
 			(Some("o"),Some("output"),true),
 		]
 	)?;
+	if args.has("h","help") {
+		print_help();
+		return Ok(());
+	}
 	let file_name = args.get_arg(Some("o"),Some("output"))
 		.unwrap_or("data.o");
 	let output_file = File::create(file_name)?;
@@ -22,18 +26,46 @@ fn main() -> Result<(),Box<dyn Error>> {
 		.name(file_name.to_string())
 		.finish();
 	//====== declare symbols ======
+	//data symbols
 	object.declarations(file_names
 		.iter()
+		.map(|f| sanitise_symbol_name(f.to_string())) //change '/' to '_'
+		.map(|f| (f,Decl::data().global().into()))
+	)?;
+	//data size symbols
+	object.declarations(file_names
+		.iter()
+		.map(|f| sanitise_symbol_name(f.to_string()) + "_size")
 		.map(|f| (f,Decl::data().global().into()))
 	)?;
 	//====== define symbols ======
 	for file_name in file_names {
-		let file_size = 5_u64;
-		let mut file_data = file_size.to_ne_bytes().to_vec();
-		file_data.append(&mut "hello".as_bytes().to_vec());
-		object.define(file_name,file_data)?
+		let file_data = read(&file_name)?;
+		let file_size = file_data.len() as u64;
+		//define the data and size symbols
+		object.define(sanitise_symbol_name(file_name.clone()) + "_size",file_size.to_ne_bytes().to_vec())?;
+		object.define(sanitise_symbol_name(file_name),file_data)?;
 	}
 	//====== write our new object ======
 	object.write(output_file)?;
 	Ok(())
+}
+
+fn sanitise_symbol_name(name: String) -> String {
+	name
+		.replace("/","_") //replace these with '_'
+		.replace("-","_")
+		.replace(".","_")
+		.chars()
+		//delete any other non alphanumeric characters
+		.filter(|c| (c.is_alphanumeric() || *c == '_') && c.is_ascii())
+		.collect()
+}
+
+fn print_help(){
+	let name = std::env::args().next().expect("No argv[0] found");
+	println!("Usage: {name} [options] <file 1> ... <file n>");
+	println!("Options:");
+	println!("	-o, --output <name>: change the name of the output file. defaults to data.o");
+	println!("	-h, --help : display this help message");
 }
