71b567da009893e921a6b5a8e555ba07a9d59a43
harry <harrywstevens4@gmail.com>
2026-09-08 17:44:46 +0100

    added create subcommand and made track subcommand try leverfile in current directory if none provided

--- lever/main.rs
+++ lever/main.rs
@@ -1,24 +1,39 @@
 mod leverfile;
 mod database;
 
+use std::process::Command;
 use leverfile::{LeverFile};
 use std::path::{Path,PathBuf};
 use database::LeverDB;
 use std::env;
 use std::process::exit;
 use std::fs::read_to_string;
+use std::fs;
+use std::io;
 
 #[derive(Clone)]
 pub struct Config {
-	pub database_path: PathBuf
+	pub database_path: PathBuf,
+	pub editor: String,
 }
 
+const DEFAULT_CONFIG: &str = "\
+# This is the default configuration file
+# Set options here
+# key=value
+# database_path=/path/to/config #defaults to ~/.config/lever/packages.ini
+editor=vim
+";
+
 impl Config {
-	pub fn new<P: AsRef<Path> + std::fmt::Debug + Clone>(config_path: Option<P>) -> Self {
+	pub fn load<P: AsRef<Path> + std::fmt::Debug + Clone>(config_path: Option<P>) -> Self {
 		//====== initialise defaults ======
-		let mut database_path = env::home_dir()
-			.map(|p| p.join(".config/lever/packages.ini"))
-			.unwrap_or(PathBuf::from("/etc/lever/packages.ini"));
+		let mut config = Self {
+			database_path: env::home_dir()
+				.map(|p| p.join(".config/lever/packages.ini"))
+				.unwrap_or(PathBuf::from("/etc/lever/packages.ini")),
+			editor: String::new(),
+		};
 		//====== load the config file if it exists ======
 		let config_file_lines = config_path.clone()
 			.map(|p| read_to_string(p).ok()) //read the config file and ignore errors
@@ -39,15 +54,17 @@ impl Config {
 					continue
 				};
 				match key.trim() {
-					"database_path" => {database_path = value.trim().into()},
+					"database_path" => {config.database_path = value.trim().into()},
+					"editor" => {config.editor = value.trim().into()},
 					_ => eprintln!("Unknown config option {:?} in {:}:{}",key,config_path.as_ref().unwrap().as_ref().display(),line_number+1)
 				}
 			}
 		}
 		//====== return the final config ======
-		Self {
-			database_path,
-		}
+		config
+	}
+	pub fn write_default(config_path: impl AsRef<Path>) -> io::Result<()> {
+		fs::write(config_path,DEFAULT_CONFIG)
 	}
 }
 
@@ -56,7 +73,15 @@ fn main(){
 	let config_path = env::home_dir()
 		.map(|p| p.join(".config/lever/lever.conf"))
 		.unwrap_or(PathBuf::from("/etc/lever/lever.conf"));
-	let config = Config::new(Some(config_path));
+	//create it if it doesnt exist
+	if !config_path.exists(){
+		if let Err(e) = Config::write_default(&config_path){
+			eprintln!("Unable to write default config file to {}: {e}",config_path.display());
+			exit(1)
+		}
+	}
+	//load the config
+	let config = Config::load(Some(config_path));
 	//====== load the database ======
 	let mut database = match LeverDB::load(&config.database_path) {
 		Ok(db) => db,
@@ -75,6 +100,7 @@ fn main(){
 		Some("install") => install(command_line[1..].into(),&config,&mut database),
 		Some("update") => update(command_line[1..].into(),&config,&mut database),
 		Some("track") => track(command_line[1..].into(),&config, &mut database),
+		Some("create") => create(command_line[1..].into(),&config,&mut database),
 		Some("help") => Ok(help()),
 		Some(command) => {
 			eprintln!("Unknown command {command:?}");
@@ -88,17 +114,73 @@ fn main(){
 }
 
 fn track(targets: Vec<String>, config: &Config, database: &mut LeverDB) -> Result<(),()> {
+	//no args provided means track leverfile in current dir
+	let targets = if targets.len() == 0 {
+		vec![String::from("leverfile.ini")]
+	}else {targets};
+	//track all the files
 	for file in targets {
-		if !Path::new(&file).is_file() || !Path::new(&file).exists() {
-			eprintln!("{:?} is not a valid leverfile",file);
-			continue;
-		}
-		let _ = database.add_tracked(file).map_err(|e| eprintln!("Error tracking in database: {e:?}"))?;
+		//
+		let leverfile = match LeverFile::load(&file){
+			Ok(leverfile) => leverfile,
+			Err(e) => {
+				eprintln!("Error loading leverfile {:?}: {e}",file);
+				continue;
+			}
+		};
+		database.add_tracked(&leverfile).map_err(|e| eprintln!("Error tracking {} in database: {e}",file))?;
 	}
 	database.save().map_err(|e| {
-		eprintln!("Error saving database: {e:?}");
+		eprintln!("Error saving database: {e}");
 		() //remove the Err content as we have already printed it
 	})
+}
+
+//TODO: create a template leverfile in current dir, open in editor and track after it is closed
+fn create(command_line: Vec<String>, config: &Config, database: &mut LeverDB) -> Result<(),()>{
+	let leverfile_path = Path::new("leverfile.ini");
+	//write the template if a leverfile doesnt already exist
+	if !leverfile_path.exists(){
+		//write the template
+		if let Err(e) = LeverFile::write_template(leverfile_path){
+			eprintln!("Failed to write leverfile template: {e}");
+			return Err(())
+		}
+	}else {
+		println!("Leverfile exists, opening...");
+	}
+	//open it in the editor for ease of user use
+	if config.editor.len() != 0 {
+		let result = Command::new(&config.editor)
+			.args([leverfile_path.to_string_lossy().into_owned()])
+			.status();
+		match result {
+			Err(e) => eprintln!("Failed to open editor: {e}"),
+			Ok(s) if !s.success() => eprintln!("Editor exited with error: {s}"),
+			Ok(_) => (),
+		}
+	}
+	//validate it
+	let leverfile = match LeverFile::load(leverfile_path){
+		Ok(leverfile) => leverfile,
+		Err(e) => {
+			eprintln!("Leverfile invalid so not tracked: {e}");
+			Err(())?
+		}
+	};
+	//track if not already tracked
+	if database.get_package_location(leverfile.name()).is_none(){
+		if let Err(e) = database.add_tracked(&leverfile){
+			eprintln!("Error tracking in database: {e}");
+			return Err(());
+		}
+		if let Err(e) = database.save(){
+			eprintln!("Error saving database: {e}");
+			return Err(());
+		}
+		println!("{} tracked",leverfile.name());
+	}
+	Ok(())
 }
 
 fn compile(targets: Vec<String>, config: &Config, database: &mut LeverDB) -> Result<(),()> {
@@ -109,14 +191,17 @@ fn compile(targets: Vec<String>, config: &Config, database: &mut LeverDB) -> Res
 		//get the path to the leverfile
 		let Some(leverfile_path) = database.get_package_location(&package)
 		else {
-			eprintln!("Could not find package {package:?}, skipping");
+			eprintln!("Couldn't find package {package:?}, skipping");
 			continue;
 		};
 		//load the leverfile
 		println!("----> Compiling {}",package);
-		let Ok(leverfile) = LeverFile::load(&leverfile_path) else {
-			eprintln!("Loading leverfile at {leverfile_path:?} failed.");
-			return Err(())
+		let leverfile = match database.get_package_leverfile(&package) {
+			Ok(lf) => lf,
+			Err(error) => {
+				eprintln!("Loading leverfile failed: {error}");
+				return Err(())
+			}
 		};
 		//determine the compile dir
 		let Some(compile_dir) = Path::new(&leverfile_path).parent()
@@ -217,4 +302,6 @@ fn help(){
 	println!("Pass it the name of packages to pull, compile then install. Passing nothing will cause it to act on all packages installed.");
 	println!("--> track");
 	println!("Pass it the path of a leverfile(s) to track");
+	println!("--> create");
+	println!("Creates a leverfile in the current directory, and opens it in an editor for you to fill in")
 }
